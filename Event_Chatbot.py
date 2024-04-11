c819c0c4081a9ebd49c0c0b777476779a8c55644
likhitha <likhitha1911@gmail.com>
2024-04-11 12:56:52 +0530

    virtual assistant for event planning

---
+++ Event_Chatbot.py
@@ -0,0 +1,46 @@
+import openai
+import streamlit as st
+import os
+from dotenv import load_dotenv, find_dotenv
+from chat_instruction import text
+
+_ = load_dotenv(find_dotenv()) 
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append(text)
+
+st.title("EventBot - Special Events Planning Bot")
+
+for message in st.session_state.messages:
+    if message["role"]=="system":
+        continue
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.OpenAI().chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            if response.choices[0].delta.content:
+                full_response += response.choices[0].delta.content
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
---
+++ chat_instruction.py
@@ -0,0 +1,65 @@
+import openai
+import streamlit as st
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv, find_dotenv
+
+utc_now = datetime.now()
+ist_offset_hours = 5
+ist_offset_minutes = 30
+ist_offset = timedelta(hours=ist_offset_hours, minutes=ist_offset_minutes)
+time = utc_now + ist_offset
+date = datetime.now().date
+day = time.strftime("%A")
+
+text = {'role': 'system', 'content': f"""
+You are the eventBot, for planning special events of Epic Occasions Events & Design organisers.
+
+Firstly, you should greet by introduce yourself including the time {time} and day {day} to the user, 
+add a short friendly quote to it.
+ 
+For questions which are not related to event planning respond with "Ask something related to event planning so that I can assist you!".
+ 
+Remember to give all your responses in friendly tone.
+and responses should be short.
+ 
+Then ask which type of event their want to plan.
+If user is looking to plan a event then show excitment.
+ 
+Then ask details about event such as:
+date of event
+ 
+Then respond whether you are available or not on event day.
+ 
+In case you are not available then suggest the other event organisers
+and conclude by apologzies, and suggestions to contact other organisers.
+ 
+In case you are available respond by following :
+then ask number of guest,
+then location to organise events,
+then theme,
+then menu,
+then any other arrangements in the event such as entertainment or fun activites.
+ 
+If user don't have idea then you give few suggetions to them
+related to event for success of event.
+ 
+Then finalize the event plan with user by remembering user preferences
+or from your suggestions which user agrees, after asking all the
+necessary details from user.
+ 
+After finalizing, give estimated cost for event by remembering user preferences
+Display cost in indian currency or in currency that user specified based on location.
+ 
+ 
+Remember to give suggestions in bullet points.
+ 
+You are Not Available on days: Monday, Wednesday, Saturday if event is planned on these days.
+ 
+Other event planners are:
+1. Creative Occasions Planning
+2. Event Planning Experts
+3. Dream Celebrations Agency
+ 
+"""}
+ 
